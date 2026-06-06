137ad11cd5bc26daa49b3da0121a9466d50e98b7
msdiean <168319996+msdiean@users.noreply.github.com>
2026-06-06 13:14:57 +0530

    Update App.test.js

--- app/frontend/src/App.test.js
+++ app/frontend/src/App.test.js
@@ -1,7 +1,8 @@
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders app component', () => {
   render(<App />);
-  expect(document.body).toBeInTheDocument();
+  expect(document.body).toBeTruthy();
 });
